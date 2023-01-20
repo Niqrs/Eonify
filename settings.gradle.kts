040edbdc46ecf77b9a5522249ed400c09dd68fd2
Niqrs <niqr.dev@gmail.com>
2023-01-20 08:10:57 +0300

    Created Splash Module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,12 +13,19 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "Eonify"
+
+// APP
 include(":app")
+// AUTH
 include(":auth:ui")
 include(":auth:data")
 include(":auth:domain")
+// PROFILE
 include(":profile:ui")
-
+// SPLASH
+include(":splash:gg")
+//CORE
 include(":core:core")
 include(":core:ui")
 include(":core:utils")
+include(":splash:ui")
