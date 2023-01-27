082cce6b2472cbf6f259d6235f2bd5143b73cbb5
Niqrs <niqr.dev@gmail.com>
2023-01-27 21:48:55 +0300

    New Typography

--- core/ui/src/main/java/com/niqr/core_ui/theme/Type.kt
+++ core/ui/src/main/java/com/niqr/core_ui/theme/Type.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
@@ -12,10 +13,33 @@ internal val Typography = Typography(
         bodyLarge = TextStyle(
                 fontFamily = FontFamily.Default,
                 fontWeight = FontWeight.Normal,
-                fontSize = 16.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.5.sp
-        )
+                fontSize = 18.sp,
+                lineHeight = 28.sp,
+                letterSpacing = 0.5.sp,
+                textAlign = TextAlign.Center
+        ),
+        bodyMedium = TextStyle(
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.5.sp,
+                lineHeight = 22.sp,
+                letterSpacing = 0.4.sp,
+                textAlign = TextAlign.Center
+        ),
+
+
+        headlineMedium = TextStyle(
+                fontWeight = FontWeight.W800,
+                fontSize = 36.sp,
+                letterSpacing = 1.sp
+        ),
+        headlineSmall = TextStyle(
+                fontWeight = FontWeight.W800,
+                fontSize = 28.sp,
+                lineHeight = 48.sp,
+                letterSpacing = 1.sp,
+                textAlign = TextAlign.Center
+        ),
         /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
