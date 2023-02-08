91964f72b8e477daa11cc1965f61c94f956d449e
Niqrs <niqr.dev@gmail.com>
2023-02-08 17:37:58 +0300

    Hilt modules update

---
+++ app/src/main/java/com/niqr/eonify/di/AppModule.kt
@@ -0,0 +1,20 @@
+package com.niqr.eonify.di
+
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthDataModule {
+
+    @Provides
+    fun provideFirebaseAuth() = Firebase.auth
+
+    @Provides
+    fun provideFirebaseFirestore() = Firebase.firestore
+}
